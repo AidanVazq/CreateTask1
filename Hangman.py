8cda4df042fa3358d6e25112b2eff03a17591808
AidanVazq Vazquez <avr.25dogs@gmail.com>
2023-09-25 04:46:38 +0000

    WOOO YEAH I ADDED A LIST

--- Hangman.py
+++ Hangman.py
@@ -1,12 +1,18 @@
-secretWord = "pasadena"
 
-#Empty list for letters that have been guessed
+import random
+#^^^^^^^^^ Im not sure what that does I just looked at what W3 schools put and it worked I guess.
+
+SecretList = ["pasadena", "roseparade", "trombone", "california", "computer", "science", "advanced", "placement", "exam"]
+
+secretWord = random.choice(SecretList)
+
+#Because empty for letters that have been guessed
 lettersguessed = ""
 
 #How many times you can be wrong. Exremely Hard >:)
-failurecount = 3
+lives = 3
 
-while failurecount > 0:
+while lives > 0:
 
     guess = input("Pick a letter to guess with")
 
@@ -14,10 +20,10 @@ while failurecount > 0:
 
         print("Good Job! Keep on guessing")
     else:
-        failurecount -= 1
+        lives -= 1
         print("Incorrct guess! Try Again")
 
-#Maintains all the guessed letters that have already been guessed in a list... IDK
+#Maintains all the guessed letters that have already been guessed. (STORES THEM UP ABOVE IN LETTERSGUESSED) I THINK...
     lettersguessed = lettersguessed + guess
     wronglettercounter = 0
 #The f in the print are for the {}
@@ -26,8 +32,9 @@ while failurecount > 0:
             print(f"{letter}", end="")
         else:
             print("_", end="")
-            wronglettercounter += 1
-
+            wronglettercounter += 1 
+    # ^^^^^^^^^ (Definition to HELP me understand end paramters) The end parameter in the print function is used to add any string
+    # At the end of the output of the print statement in python.
     if wronglettercounter == 0:
         print(f" Nice Work! The secret word was: {secretWord}!")
         break
